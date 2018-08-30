aa508dbfa21f93fd12c6f1e3ae5bcfe2ecc72855
fwahlberg <wahlbergf100@gmail.com>
2018-08-30 21:09:58 +0100

    Added project page

--- server.js
+++ server.js
@@ -7,26 +7,26 @@ var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
   var now = new Date().toString();
   var log = `${now}: ${req.method} ${req.url}`;
+
   console.log(log);
-  fs.appendFile('server.log', log + '\n', (err) => {
-    if (err) {
-      console.log('Unable to write server log');
-    }
+  fs.appendFile('server.log', log + '\n', (error) => {
+    console.log(error);
   });
   next();
 });
 
-app.use((req,res,next) => {
-  res.render('maintenance.hbs');
-});
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
-  return new Date().getFullYear()
+  return new Date().getFullYear();
 });
 
 hbs.registerHelper('screamIt', (text) => {
@@ -37,6 +37,13 @@ app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
     welcomeMessage: 'Welcome to my website'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Projects Page',
+    welcomeMessage: 'Project Page here'
   });
 });
 
